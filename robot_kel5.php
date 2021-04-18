469107262bf9c3b8e0fb59997668a68797c72178
andrapn <andraput988@gmail.com>
2021-04-18 09:04:30 +0700

    Commit oleh Andra

---
+++ robot_kel5.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Robot Kelompok 5</title>
+    <style>
+        *{
+            padding:0;
+            margin:0;
+        }
+        .container{
+            margin:10px auto;
+            width:600px;
+            border:2px solid salmon;
+            text-align:center;
+            padding:10px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <img src="https://i.pinimg.com/736x/9d/c0/a7/9dc0a74078c72e57de35b419a6a7d0c0.jpg" alt="robot_kel5" width=200 height=200>
+
+    <h1>Robot Kelompok 5</h1>
+    <br>
+    <form method="POST">
+        <input type="submit" name="nyala" value="NYALAKAN ROBOT">
+    </form>
+</div>
